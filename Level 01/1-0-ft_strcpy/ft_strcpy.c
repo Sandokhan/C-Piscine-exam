26f3b83b07566befab34c336c3750d5367d2c0fa
sandokhan <rhaydrick@gmail.com>
2025-01-23 22:22:25 +0000

    Level 01 ft_strcpy

--- Level 01/1-0-ft_strcpy/ft_strcpy.c
+++ Level 01/1-0-ft_strcpy/ft_strcpy.c
@@ -1,40 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/26 12:48:30 by angavrel          #+#    #+#             */
-/*   Updated: 2017/07/14 14:50:20 by fwuensch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <unistd.h>
 
-char    *ft_strcpy(char *s1, char *s2)
+char *ft_strcpy(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s2[i])
+	while (s2[i]!= '\0')
 	{
 		s1[i] = s2[i];
 		i++;
 	}
-	s1[i] = s2[i];
+	s1[i] = '\0';
 	return (s1);
 }
-
-// DO NOT SUBMIT ANYTHING BELOW
 
 #include <stdio.h>
 
 int main(void)
 {
-	char str1[] = "Flavio";
-	char str2[] = "Gaucho";
+	char src[] = "Hello!";
+	char dest[20];
 
-	printf("%s\n", str1);
-	ft_strcpy(str1, str2);
-	printf("%s\n", str1);
-	return (0);
+	printf("Src: %s\n Dest: %s - Initial state\n", src, dest);
+	printf("Dest: %s - final state", ft_strcpy(dest, src));
+	return(0);
+	
 }
