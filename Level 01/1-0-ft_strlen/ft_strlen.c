8d4863adad523e413bbcdd220366ad15799f9fe7
sandokhan <rhaydrick@gmail.com>
2025-01-23 22:40:06 +0000

    Level 01 ft_strlen

--- Level 01/1-0-ft_strlen/ft_strlen.c
+++ Level 01/1-0-ft_strlen/ft_strlen.c
@@ -1,33 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/14 14:51:14 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/14 14:52:08 by fwuensch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <unistd.h>
 
-int		ft_strlen(char *str)
+int ft_strlen( char *str)
 {
 	int i;
-
+	
 	i = 0;
-	while(str[i])
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
 
-// DO NOT SUBMIT ANYTHING BELOW
-
 #include <stdio.h>
-
-int main (void)
+int main(void)
 {
-	char str[] = "Flavio";
-
-	printf("%d\n", ft_strlen(str));
+	char name[] = "Rhaydrick";
+	printf("%s have size %d", name, ft_strlen(name));
 	return (0);
 }
