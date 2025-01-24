9be607072be488359b399e4bea340e82a9cbd55f
sandokhan <rhaydrick@gmail.com>
2025-01-24 07:58:15 +0000

    feat: add search_and_replace

--- Level 01/1-0-search_and_replace/search_and_replace.c
+++ Level 01/1-0-search_and_replace/search_and_replace.c
@@ -1,41 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   search_and_replace.c                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/14 15:09:23 by fwuensch          #+#    #+#             */
-/*   Updated: 2017/07/14 15:09:24 by fwuensch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int main(int argc, char **argv) {
-	int i;
-
-	// we only need three parameters
-	if (argc != 4) {
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
 		write(1, "\n", 1);
-		return 0;
+		return (0);
 	}
-
-	// the second and third arguments must be letters
-	if (argv[2][1] != '\0' || argv[3][1] != '\0') {
-		write(1, "\n", 1);
-		return 0;
+	if (argv[2][1] != '\0' || argv[3][1] != '\0')
+    {
+        write(1, "\n", 1);
+        return (0);
+    }
+	while (*argv[1])
+	{
+		if (*argv[1] == *argv[2])
+			write(1, argv[3], 1);
+		else
+			write(1, argv[1], 1);
+		argv[1]++;
 	}
-
-	// find and replace
-	i = 0;
-	while (argv[1][i] != '\0') {
-		if (argv[1][i] == argv[2][0])
-			argv[1][i] = argv[3][0];
-		write(1, &argv[1][i], 1);
-		i++;
-	}
-
 	write(1, "\n", 1);
-	return 0;
+	return (0);
 }
