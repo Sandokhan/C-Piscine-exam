ef45af22465afed5e34fb4ccccc8435d3c8b5958
sandokhan <rhaydrick@gmail.com>
2025-01-28 19:15:47 +0000

    feat: add ulstr

--- Level 01/1-0-ulstr/ulstr.c
+++ Level 01/1-0-ulstr/ulstr.c
@@ -1,36 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ulstr.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/03 15:11:26 by angavrel          #+#    #+#             */
-/*   Updated: 2017/07/14 15:28:42 by fwuensch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int		main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	if (ac == 2)
+	char letter;
+
+	if (argc != 2)
 	{
-		while (*av[1])
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (*argv[1])
+	{
+		if (*argv[1] >= 'a' && *argv[1] <= 'z')
 		{
-			if (*av[1] >= 'A' && *av[1] <= 'Z')
-			{
-				*av[1] += 32;
-				write(1, av[1], 1);
-			}
-			else if (*av[1] >= 'a' && *av[1] <= 'z')
-			{
-				*av[1] -= 32;
-				write(1, av[1], 1);
-			}
-			av[1]++;
+			letter = *argv[1] - 32;
+			write(1, &letter, 1);
 		}
+		else if (*argv[1] >= 'A' && *argv[1] <= 'Z')
+		{
+			letter = *argv[1] + 32;
+			write(1, &letter, 1);
+		}
+		else
+		{
+			write(1, argv[1], 1);
+		}
+		argv[1]++;
 	}
 	write(1, "\n", 1);
-	return (1);
+	return (0);
 }
